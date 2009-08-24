fed54ba9a68d0956f5c336b77afdfd612c74efbd
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2009-08-24 13:57:25 +0000

    tburguie: j'enleve l'ancien dossier avec l'ancienne version du connecteur
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@563 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/simulateur_interface_utilisateur.sh
+++
@@ -1,6 +0,0 @@
-killall -9 socat  &> /dev/null
-rm -f /tmp/testW
-rm -f /tmp/test2W
-socat -u  UNIX-LISTEN:/tmp/testW,fork  STDOUT &
-socat -u  UNIX-LISTEN:/tmp/test2W,fork  STDOUT &
-
--- src/simulateur_nagios.sh
+++
@@ -1,3 +0,0 @@
-for file in error.txt event_incomplet.txt event.txt perf.txt ; do 
-    cat  $file | socat - UNIX-CONNECT:/tmp/testR
-done
