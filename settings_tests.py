0c5a57f0ac324b9125f97cc4d71575cc869fb6b3
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2009-09-09 12:44:21 +0000

    tburguie: modif parametre de configuration
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@688 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- settings_tests.py
+++ settings_tests.py
@@ -4,18 +4,20 @@ LOGGING_SETTINGS = { 'level': logging.DEBUG, }
 LOGGING_LEVELS = {}
 LOGGING_SYSLOG = False
 
+
+
 VIGILO_CONNECTOR_DAEMONIZE = True
 VIGILO_CONNECTOR_PIDFILE = '/var/lib/vigilo/connector/connector.pid'
-VIGILO_CONNECTOR_XMPP_SERVER_HOST = 'localhost'
-VIGILO_CONNECTOR_XMPP_PUBSUB_SERVICE = 'pubsub.localhost'
+VIGILO_CONNECTOR_XMPP_SERVER_HOST = 'tburguie3'
+VIGILO_CONNECTOR_XMPP_PUBSUB_SERVICE = 'pubsub.tburguie3'
 # Respect the ejabberd namespacing, for now. It will be too restrictive soon.
-VIGILO_CONNECTOR_JID = 'connector@localhost'
-VIGILO_CONNECTOR_PASS = 'connector'
+VIGILO_CONNECTOR_JID = 'connectorx@tburguie3'
+VIGILO_CONNECTOR_PASS = 'connectorx'
 
-VIGILO_CONNECTOR_TOPIC = '/home/localhost/connector/alerts'
+VIGILO_CONNECTOR_TOPIC = '/home/tburguie3/connectorx/BUS'
 VIGILO_SOCKETW = '/var/lib/vigilo/connector/recv.sock'
-#VIGILO_SOCKETW = None
 VIGILO_SOCKETR = '/var/lib/vigilo/connector/send.sock'
-VIGILO_MESSAGE_BACKUP_FILE = '/tmp/backup'
-VIGILO_MESSAGE_BACKUP_TABLE = 'connector'
+VIGILO_MESSAGE_BACKUP_FILE = '/var/lib/vigilo/connector/backup'
+VIGILO_MESSAGE_BACKUP_TABLE_TOBUS = 'connector_tobus'
+VIGILO_MESSAGE_BACKUP_TABLE_FROMBUS = 'connector_frombus'
 
