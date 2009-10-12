219167422833540511a79b6f8e9227d67075ed69
Francois POIROTTE <francois.poirotte@c-s.fr>
2009-10-12 11:14:06 +0000

    Ajout de documentation dans la classe DbRetry.
    ATTENTION : modification du comportement, unstore() renvoie None à la place de True/False lorsqu'aucun message ne peut être renvoyé. La méthode propage également les exceptions lorsqu'elles se produisent.
    
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@871 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/connector/store.py
+++ src/vigilo/connector/store.py
@@ -13,9 +13,26 @@ _ = translate(__name__)
 
 
 class DbRetry(object):
+    """
+    Implémente une base de données locale qui peut-être utilisée pour stocker
+    des messages XML lorsque le destinataire final n'est pas joignable.
+    """
+
     def __init__(self, filename, table):
+        """
+        Instancie une base de données locale pour la réémission des messages.
+
+        @param filename: Le nom du fichier qui contiendra la base de données.
+        @type filename: C{str}
+        @param table: Le nom de la table SQL dans ce fichier.
+        @type table: C{str}
+        """
+
         self.__table = table
-        self.__connection = sqlite.connect(filename)
+        # XXX Il faudrait s'assurer que les différents composants
+        # sont thread-safe, car check_same_thread=False désactive
+        # toutes les vérifications faites normalement par pysqlite.
+        self.__connection = sqlite.connect(filename, check_same_thread=False)
         cursor = self.__connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS %s \
                 (id INTEGER PRIMARY KEY, msg TXT)' % table)
@@ -23,9 +40,21 @@ class DbRetry(object):
         cursor.close()
 
     def __del__(self):
+        """Libère la base de données locale."""
         self.__connection.close()
 
     def store(self, msg):
+        """ 
+        Stocke un message XML dans la base de données locale en attendant
+        de pouvoir le retransmettre à son destinataire finale. 
+
+        @param msg: Le message à stocker.
+        @type  msg: C{str}
+        @return: Un booléen indiquant si l'opération a réussi ou non.
+        @rtype: C{bool}
+        @raise sqlite.OperationalError: Une exception levée par sqlite.
+        """
+
         cursor = self.__connection.cursor()
 
         try:
@@ -47,14 +76,23 @@ class DbRetry(object):
         return False
 
     def unstore(self):
-        msg = None
+        """
+        Renvoie un message issu de la base de données locale.
+        Utilisez cette méthode lorsque la connexion avec le destinataire
+        du message est rétablie pour retransmettre les données.
+
+        @return: Un message stocké en attente de retransmission
+            ou None lorsqu'il n'y a plus de message.
+        @rtype: C{str} ou C{None}
+        @raise sqlite.OperationalError: Une erreur possible 
+        @raise sqlite.Error:
+        """
+
         cursor = self.__connection.cursor()
         cursor.execute('SELECT MIN(id) FROM %s' % self.__table)
         id_min = cursor.fetchone()[0]
-        empty = True
         
         if id_min:
-            empty = False
             try:
                 cursor.execute('SELECT msg FROM %s WHERE id = ?' %
                     self.__table, (id_min,))
@@ -69,23 +107,31 @@ class DbRetry(object):
                 self.__connection.rollback()
                 cursor.close()
 
-                if e.__str__() == "database is locked":
-                    LOGGER.warning(_(e.__str__()))
+                if str(e) == "database is locked":
+                    LOGGER.warning(_("The database is locked"))
                     time.sleep(5)
                     return self.unstoremessage()
                 else:
-                    LOGGER.error(_(e.__str__()))
+                    LOGGER.error(
+                        _("Some operational error occured. " +
+                          "Original error message was: %s") %
+                        str(e))
                     raise e
  
             except sqlite.Error, e:
                 self.__connection.rollback()
                 cursor.close()
-                LOGGER.warning(_(e.__str__()))
+                LOGGER.error(
+                    _("Some error occured. " +
+                      "Original error message was: %s") %
+                    str(e))
+                raise e
 
         cursor.close()
-        return empty
+        return None
 
     def vacuum(self):
+        """Effectue une optimisation de la base de données."""
         cursor = self.__connection.cursor()
         cursor.execute('VACUUM')
         self.__connection.commit()
