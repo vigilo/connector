f088d50af3cf5b6ab9cfb88574b1537c7e38afbc
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-02-18 15:14:40 +0000

    Correction des setup.py : - author = "Vigilo Team" - author_email = "contact@projet-vigilo.org" - license = GPLv2+
    Ajout des fichiers COPYING dans les composants qui ne l'avaient pas encore.
    
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2085 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ tests_require = [
 
 setup(name='vigilo-connector',
         version='0.1',
-        author='Thomas BURGUIERE',
-        author_email='thomas.burguiere@c-s.fr',
+        author='Vigilo Team',
+        author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
         description='vigilo connector component',
         license='http://www.gnu.org/licenses/gpl-2.0.html',
         long_description='The vigilo connector component is a connector between:\n'
         +'   - XMPP/PubSub BUS of message\n'
         +'   - UNIX sockets\n'
-        +'(there is two differents sockets, one for the message socket to BUS, one for the message BUS to socket\n',
+        +'(there are two sockets, for incoming and outgoing messages)\n',
         install_requires=[
             # dashes become underscores
             # order is important (wokkel before Twisted)
