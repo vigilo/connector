16b27137732509595ee48af9e88c490372c7a228
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-03-08 15:14:54 +0000

    Gestion des erreurs d'authentification
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2299 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/connector/client.py
@@ -0,0 +1,34 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.words.protocols.jabber import xmlstream
+from twisted.words.protocols.jabber.sasl import SASLNoAcceptableMechanism,SASLAuthError
+
+from wokkel import client
+
+class XMPPClient(client.XMPPClient):
+    """Client XMPP Vigilo"""
+
+    def __init__(self, *args, **kw):
+        client.XMPPClient.__init__(self, *args, **kw)
+
+    def _disconnected(self, xs):
+        """
+        Ajout de l'arrêt à la déconnexion
+        @TODO: vérifier que ça ne bloque pas la reconnexion automatique.
+        """
+        client.XMPPClient._disconnected(self, xs)
+        reactor.stop()
+
+    def initializationFailed(self, failure):
+        """
+        Appelé si l'initialisation échoue. Ici, on ajoute la gestion de
+        l'erreur d'authentification.
+        """
+        if failure.check(SASLNoAcceptableMechanism,SASLAuthError):
+            log.err(failure, "Authentication failed:")
+            self.stopService()
+            return
+        client.XMPPClient.initializationFailed(self, failure)
+
