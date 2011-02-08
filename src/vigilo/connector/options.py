14706c6b81e9d2ba8e6259c5b14b604b3f48e8c9
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-02-08 11:19:32 +0000

    connecteurs: adaptation des messages d'état au lancement par twistd
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6008 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/connector/options.py
+++ src/vigilo/connector/options.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+import sys
+
 from twisted.python import usage
 
 class Options(usage.Options):
+
     def opt_version(self):
+        """Display the version and exit"""
         print 'Vigilo 2.0.0 (svn%s)' % ('$Rev: 5998 $'[6:-2])
         sys.exit()
+
+    def opt_name(self, name):
+        """Choose the service name"""
+        self["name"] = name
+
