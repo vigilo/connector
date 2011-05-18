8dc22bc2972218491217d2a88c10aeceb5a858f4
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-05-18 13:35:57 +0000

    MAJ des versions
    (buildbot: no-build)
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@7120 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def install_i18n(i18ndir, destdir):
     return data_files
 
 setup(name='vigilo-connector',
-        version='2.0.0',
+        version='2.0.1',
         author='Vigilo Team',
         author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
