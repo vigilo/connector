0365e3de599a81129c5e1c95247d87f85ecfd8ea
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-10 16:32:15 +0100

    Synchro des settings_tests.ini (ou suppression si inutile)
    Change-Id: I896df162bdd9db4633210e84448fa84a5d4c3907

--- src/vigilo/connector/test/__init__.py
+++ src/vigilo/connector/test/__init__.py
@@ -1,5 +1,3 @@
 from nose.twistedtools import reactor, stop_reactor
-from vigilo.common.conf import settings
-settings.load_module(__name__)
 from vigilo.common.logging import get_logger
 LOGGER = get_logger(__name__)
