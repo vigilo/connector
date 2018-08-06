31e0acee959e770a65b3c759177064d1a47f53c3
Francois POIROTTE <francois.poirotte@c-s.fr>
2018-08-06 17:55:19 +0200

    Préparation Vigilo v5.0.0
    Change-Id: I5e13f54932b862b82e436e8a74ff8d3fabc1fd09

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def install_i18n(i18ndir, destdir):
     return data_files
 
 setup(name='vigilo-connector',
-        version='4.1.0',
+        version='5.0.0',
         author='Vigilo Team',
         author_email='contact.vigilo@c-s.fr',
         url='https://www.vigilo-nms.com/',
