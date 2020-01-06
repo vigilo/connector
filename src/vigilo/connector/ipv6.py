9c8412e66c814d7d298ad2169fab6e0052d79f84
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-01-06 12:43:33 +0100

    Mise à jour des copyrights
    Change-Id: I7f8627fe62e606a6a5f9a2118bc05091bcf879e1

--- src/vigilo/connector/ipv6.py
+++ src/vigilo/connector/ipv6.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
-# Copyright (C) 2011-2019 CS-SI
+# Copyright (C) 2011-2020 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 """
 Contient des extensions pour les classes de Twisted
